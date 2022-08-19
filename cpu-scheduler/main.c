8ba1d746cef4f5bec3e2f9cc998e16b63923b22a
void-developer <angeli_leonardo@icloud.com>
2022-08-19 10:56:05 +0200

    Initial Commit

---
+++ cpu-scheduler/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  cpu-scheduler
+//
+//  Created by Leonardo Angeli on 19/08/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
