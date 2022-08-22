c379c56b10600bcf9277b65e285ac454afae9c0d
void-developer <angeli_leonardo@icloud.com>
2022-08-22 13:45:33 +0200

    [doc] Added documentation for queues and some utility files

--- cpu-scheduler/includes/queues.c
+++ cpu-scheduler/includes/queues.c
@@ -10,16 +10,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ Initializes the queue with the default parameters.
+ Initial size is 0, head and tail are both NULL
+ @param q queue to initialize
+*/
 void init(queue *q) {
     q->size = 0;
     q->head = NULL;
-    q->head = NULL;
+    q->tail = NULL;
 }
 
+/**
+ Checks whether the given queue is empty
+ @param q queue
+ */
 int is_empty(queue *q) {
     return (q->head == NULL);
 }
 
+/**
+ Adds a process to the tail of the queue
+ @param q queue
+ @param p the process to add to the queue
+*/
 void enqueue(queue *q, process *p) {
     node *new_node;
     new_node = (node *) malloc(sizeof(node));
@@ -36,6 +50,10 @@ void enqueue(queue *q, process *p) {
     return;
 }
 
+/**
+ Removes the top element of the queue and returns it from the function
+ @param q queue
+*/
 process *dequeue(queue *q) {
     node *tmp = q->head;
     process *p = tmp->process;
